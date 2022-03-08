f7951fd20a662878cf741424c4b6308d21a89a9c
Miguel <>
2022-03-08 18:18:42 -0400

    Cita

--- src/main/java/Modelo/Entidades/Cita.java
+++ src/main/java/Modelo/Entidades/Cita.java
@@ -1,13 +1,77 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modelo.Entidades;
 
-/**
- *
- * @author Hogar
- */
 public class Cita {
+    private String ID_Paciente;
+    private String ID_Medico;
+    private String ID_Sucursal;
+    private String Fecha;
+    private char Status;
     
+    public Cita(){
+        ID_Paciente = "";
+        ID_Medico = "";
+        ID_Sucursal = "";
+        Fecha = "";
+        Status = Character.MIN_VALUE;
+    }
+    
+    public Cita(String id, String id2, String id3, String fecha, char status){
+        ID_Paciente = id;
+        ID_Medico = id2;
+        ID_Sucursal = id3;
+        Fecha = fecha;
+        Status = status;
+    }
+    
+    public void Solicitar(){
+        
+    }
+    
+    public void Modificar(Cita cita){
+        
+    }
+    
+    public void Cancelar(Cita cita){
+        
+    }
+    
+    public void setID_Paciente(String id){
+        ID_Paciente = id;
+    }
+    
+    public String getID_Paciente(){
+        return ID_Paciente;
+    }
+    
+    public void setID_Medico(String id){
+        ID_Medico = id;
+    }
+    
+    public String getID_Medico(){
+        return ID_Medico;
+    }
+    
+    public void setID_Sucursal(String id){
+        ID_Sucursal = id;
+    }
+    
+    public String getID_Sucursal(){
+        return ID_Sucursal;
+    }
+    
+    public void setFecha(String fecha){
+        Fecha = fecha;
+    }
+    
+    public String getFecha(){
+        return Fecha;
+    }
+    
+    public void setStatus(char status){
+        Status = status;
+    }
+    
+    public char getStatus(){
+        return Status;
+    }
 }
