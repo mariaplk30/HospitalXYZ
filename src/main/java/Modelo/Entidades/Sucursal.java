d752a513e5176d53a59999acd691e12608612628
freddygl3 <freddygl3@dhcppc3>
2022-03-15 21:12:26 -0400

    Solucionando el error de miguel, y algunos cambios a ctrl_cita

--- src/main/java/Modelo/Entidades/Sucursal.java
+++ src/main/java/Modelo/Entidades/Sucursal.java
@@ -1,12 +1,32 @@
 package Modelo.Entidades;
-import java.util.*;
+import java.util.ArrayList;
 
-public class Sucursal {
+public class Sucursal{
+    private String Nombre;
     private String Ubicacion;
     private String Telefono;
-    private String Gerente;
-    private int CantidadDepartamentos;
-    private ArrayList<String> Departamentos;
+    private ArrayList<Medico> Medicos;
+    private ArrayList<Paciente> Pacientes;
+    private ArrayList<Cita> Citas;
+    private ArrayList<Historia> Historias;
+    
+    public Sucursal(){
+        Nombre = "";
+        Ubicacion = "";
+        Telefono = "";
+        Medicos = new ArrayList();
+        Pacientes = new ArrayList();
+        Citas = new ArrayList();
+        Historias = new ArrayList();
+    }
+    
+    public void setNombre(String nombre){
+        Nombre = nombre;
+    }
+    
+    public String getNombre(){
+        return Nombre;
+    }
     
     public void setUbicacion(String ubi){
         Ubicacion = ubi;
@@ -23,40 +43,12 @@ public class Sucursal {
     public String getTelefono(){
         return Telefono;
     }
-            
-    public void setGerente(String gerente){
-        Gerente = gerente;
-    }
-
-    public String getGerente(){
-        return Gerente;
+    
+    public void addMedico(Medico medico){
+        Medicos.add(medico);
     }
             
-    public void setCantDepartamentos(int num){
-        CantidadDepartamentos = num;
-    }
-
-    public int getCantDepartamentos(){
-        return CantidadDepartamentos;
-    }
-    
-    public void setDepartamentos(int num, String depa){
-        if(num<=CantidadDepartamentos){
-            Departamentos.add(num, Telefono);
-        }
-    }
-    
-    public String getDepartamentos(int num){
-        return Departamentos.get(num);
-    }
-    
-    public ArrayList<String> getDepartamentos(){
-        return Departamentos;
-    }
-    
-    public void ListarDepartamentos(){
-        for(int i=0;i<CantidadDepartamentos;i++){
-            System.out.println(Departamentos.get(i));
-        }
+    public void addPaciente(Paciente paciente){
+        Pacientes.add(paciente);
     }
 }
