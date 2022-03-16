3b1e14b2be3a3cf32d6a6f6ce04022395d97b747
Miguel <>
2022-03-16 09:35:08 -0400

    Acomodé los IDs de Junta, Médico, Historia

--- src/main/java/Modelo/Entidades/Cita.java
+++ src/main/java/Modelo/Entidades/Cita.java
@@ -2,35 +2,29 @@ package Modelo.Entidades;
 
 public class Cita{
     private String ID;
-    private String ID_Medico;
     private String Fecha;
     
     public Cita(){
         ID = "";
-        ID_Medico = "";
         Fecha = "";
     }
     
-    public Cita(String id, String id2, String id3, String fecha){
+    public Cita(String id){
         ID = id;
-        ID_Medico = id2;
+        Fecha = "";
+    }
+    
+    public Cita(String id, String fecha){
+        ID = id;
         Fecha = fecha;
     }
     
-    public void setID_Paciente(String id){
+    public void setID(String id){
         ID = id;
     }
     
-    public String getID_Paciente(){
+    public String getID(){
         return ID;
-    }
-    
-    public void setID_Medico(String id){
-        ID_Medico = id;
-    }
-    
-    public String getID_Medico(){
-        return ID_Medico;
     }
     
     public void setFecha(String fecha){
