9ec0c88a2c546f8a71fb779aa71644ba09b4163e
David <40966434+Daviddp96@users.noreply.github.com>
2022-03-15 22:34:21 -0400

    Cont. FabricaSucursales. En JuntaDirectiva revisar no logro que .contains sirva

--- src/main/java/Modelo/Entidades/JuntaDirectiva.java
+++ src/main/java/Modelo/Entidades/JuntaDirectiva.java
@@ -9,13 +9,23 @@ public class JuntaDirectiva{
     private ArrayList<Medico> RegistroMedicos;
     private ArrayList<Sucursal> RegistroSucursales;
     
-    public JuntaDirectiva(){
+    private FabricaSucursales fabricaSucursales;
+    
+    public JuntaDirectiva(FabricaSucursales fabricaSucursales){
         Directivos = new ArrayList();
         CantidadPacientes = 0;
         PacientesAnuales = new ArrayList();
         CantidadMedicos = 0;
         RegistroMedicos = new ArrayList();
         RegistroSucursales = new ArrayList();
+        this.fabricaSucursales = fabricaSucursales;
+    }
+    
+    public void abrirSucursal(String nombre) {
+        Sucursal sucursal = fabricaSucursales.crearSucursal(nombre);
+        //Revisar si en sucursales hay otra con el mismo nombre, no logro que .contains sirva
+        //Si no, pushearla a la lista
+        return;
     }
     
     public void setDirectivo(String nombre){
