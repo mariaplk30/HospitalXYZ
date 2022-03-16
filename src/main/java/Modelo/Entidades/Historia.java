8bf825d433334891024c42c7fe5ebe69d866e1ce
Miguel <>
2022-03-16 10:49:59 -0400

    push

--- src/main/java/Modelo/Entidades/Historia.java
+++ src/main/java/Modelo/Entidades/Historia.java
@@ -1,21 +1,55 @@
 package Modelo.Entidades;
 import java.util.ArrayList;
-
+/**/
 public class Historia{
+    private String ID;
+    private String Ci;
     private int Frecuencia;
     private ArrayList<Cita> ControlDeCita;
     private ArrayList<String> Evolucion;
     private ArrayList<String> Tratamientos;
     
     public Historia(){
+        ID = "";
+        Ci = "";
         Frecuencia = 0;
         ControlDeCita = new ArrayList();
         Evolucion = new ArrayList();
         Tratamientos = new ArrayList();
     }
     
-    public void setFrecuencia(int frecuencia){
-        Frecuencia = frecuencia;
+    public Historia(String ci){
+        ID = "";
+        Ci = ci;
+        Frecuencia = 0;
+        ControlDeCita = new ArrayList();
+        Evolucion = new ArrayList();
+        Tratamientos = new ArrayList();
+    }
+    
+    public Historia(String id, String ci){
+        ID = id;
+        Ci = ci;
+        Frecuencia = 0;
+        ControlDeCita = new ArrayList();
+        Evolucion = new ArrayList();
+        Tratamientos = new ArrayList();
+    }
+    
+    public void setID(String id){
+        ID = id;
+    }
+    
+    public String getID(){
+        return ID;
+    }
+    
+    public void setCI(String ci){
+        Ci = ci;
+    }
+    
+    public String getCI(){
+        return Ci;
     }
     
     public int getFrecuencia(){
@@ -24,24 +58,30 @@ public class Historia{
     
     public void GuardarCita(Cita cita){
         ControlDeCita.add(Frecuencia, cita);
+        Frecuencia++;
     }
     
-    public Cita getControlDeCita(int i){
-        return ControlDeCita.get(i);
+    public void GuardarCita(String id, String fecha){
+        Cita cita = new Cita(id, fecha); 
+        GuardarCita(cita);
     }
     
-    public void setEvolucion(int nCita,String valores){
-        Evolucion.add(nCita, valores);
+    public Cita getCita(int nCita){
+        return ControlDeCita.get(nCita);
     }
-            
+    
+    public void setEvolucion(String valores){
+        Evolucion.add(Frecuencia,valores);
+    }
+        
     public String getEvolucion(int nCita){
         return Evolucion.get(nCita);
     }
-            
-    public void setTratamientos(int nCita, String recipe){
-        Tratamientos.add(nCita, recipe);
+    
+    public void setTratamientos(String recipe){
+        Tratamientos.add(Frecuencia, recipe);
     }
-            
+       
     public String getTratamientos(int nCita){
         return Tratamientos.get(nCita);
     }
