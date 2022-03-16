c84d035d899b25ad4bffca9fe7a1d061a609d6e1
Miguel <>
2022-03-16 10:46:54 -0400

    push

--- src/main/java/Modelo/Entidades/Sucursal.java
+++ src/main/java/Modelo/Entidades/Sucursal.java
@@ -8,9 +8,11 @@ public class Sucursal{
     private String Telefono;
     private ArrayList<Medico> Medicos;
     private ArrayList<Paciente> Pacientes;
-    private ArrayList<Cita> Citas;
-    private ArrayList<Historia> Historias;
+    private ArrayList<Cita> Citas; //Se guardan todas las citas solicitadas para asignarles un ID único y luego
+                                   //poder operar a gusto con las citas
     
+    private ArrayList<Historia> Historias; //Se guardan todas las historias registradas para asignarles un ID
+                                           //único y luego ingresarla en su paciente correspondiente.
     public Sucursal(){
         ID = "";
         Nombre = "";
@@ -18,15 +20,24 @@ public class Sucursal{
         Telefono = "";
         Medicos = new ArrayList();
         Pacientes = new ArrayList();
-        Citas = new ArrayList();
         Historias = new ArrayList();
     }
     
-    public void setID_Sucursal(String id){
+    public Sucursal(String nombre){
+        Nombre = nombre;
+        ID = "";
+        Ubicacion = "";
+        Telefono = "";
+        Medicos = new ArrayList();
+        Pacientes = new ArrayList();
+        Historias = new ArrayList();
+    }
+    
+    public void setID(String id){
         ID = id;
     }
     
-    public String getID_Sucursal(){
+    public String getID(){
         return ID;
     }
     
@@ -54,13 +65,34 @@ public class Sucursal{
         return Telefono;
     }
     
+    public int cantMedicos(){
+        return Medicos.size();
+    }
+    
     public void addMedico(Medico medico){
         Medicos.add(medico);
+    }
+    
+    public void addMedico(String nombre, String especialidad){
+        Medico medico = new Medico(nombre, especialidad);
+        addMedico(medico);
     }
     
     public void addMedico(String id, String nombre, String especialidad){
         Medico medico = new Medico(id, nombre, especialidad);
         addMedico(medico);
+    }
+    
+    public Medico getMedico(int i){
+        return Medicos.get(i);
+    }
+    
+    public ArrayList<Medico> getMedicos(){
+        return Medicos;
+    }
+    
+    public int cantPacientes(){
+        return Pacientes.size();
     }
     
     public void addPaciente(Paciente paciente){
@@ -70,5 +102,79 @@ public class Sucursal{
     public void addPaciente(String ci, String nombre){
         Paciente paciente = new Paciente(ci, nombre);
         addPaciente(paciente);
+    }
+    
+    public Paciente getPaciente(int i){
+        return Pacientes.get(i);
+    }
+    
+    public ArrayList<Paciente> getPacientes(){
+        return Pacientes;
+    }
+    
+    public int cantHistorias(){
+        return Historias.size();
+    }
+    
+    //ID_Cita emplea la misma lógica de los métodos ID_X de la clase BDD, cambiando la clase utilizada.
+    private String ID_Cita(){
+        String mensaje = "";
+        int j = 0;
+        
+        if(Math.log10(Citas.size())<1) j = 1;
+        else j = (int) Math.log10(Citas.size()) + 1;
+        
+        for(int i=1; i<=8-j; i++) mensaje += "0";
+        
+        return mensaje + Integer.toString(Citas.size() - 1);  
+    }
+    
+    public void addCita(Cita cita){
+        Citas.add(cita);
+        Citas.get(Citas.size()-1).setID(ID_Cita()); //A cada cita agendada se le genera su ID único.
+    }
+    
+    public void addCita(String id){
+        Cita cita = new Cita(id);
+        addCita(cita);
+    }
+    
+    public Cita getCita(int i){
+        return Citas.get(i);
+    }
+    
+    public ArrayList<Cita> getCitas(){
+        return Citas;
+    }
+  
+    //ID_Historia emplea la misma lógica de los métodos ID_X antes vistos, cambiando la clase utilizada.
+    private String ID_Historia(){
+        String mensaje = "H";
+        int j = 0;
+        
+        if(Math.log10(Historias.size())<1) j = 1;
+        else j = (int) Math.log10(Historias.size()) + 1;
+        
+        for(int i=1; i<=8-j; i++) mensaje += "0";
+        
+        return mensaje + Integer.toString(Historias.size() - 1);  
+    }
+    
+    public void addHistoria(Historia historia){
+        Historias.add(historia);
+        Historias.get(Historias.size()-1).setID(ID_Historia()); //A cada historia agendada se le genera su ID único.
+    }
+    
+    public void addHistoria(String ci){
+        Historia historia = new Historia(ci);
+        addHistoria(historia);
+    }
+    
+    public Historia getHistoria(int i){
+        return Historias.get(i);
+    }
+    
+    public ArrayList<Historia> getHistorias(){
+        return Historias;
     }
 }
