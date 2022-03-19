e9c44094f99d17fd1d00cdb0f1f84ef8f330d205
Miguel <>
2022-03-19 19:05:41 -0400

    Comentarios en Ctrl_JuntaDirectiva

--- src/main/java/Controlador/Ctrl_JuntaDirectiva.java
+++ src/main/java/Controlador/Ctrl_JuntaDirectiva.java
@@ -12,19 +12,26 @@ public class Ctrl_JuntaDirectiva {
     private Medico RegistroMedicos;
 
     public void RegistrarMedico(){
-    
+        if(!ExisteMedico()){
+            
+        }else{
+            
+        }
     }
 
-    public boolean ExisteSucursal(){
+    public boolean ExisteSucursal(String nombre){
+        //Se tiene que verificar en bdd si existe la sucursal a crear
         return true;
     }
 
     public boolean AbrirSucursal(String nombre){
-        if(!ExisteSucursal()){
+        if(!ExisteSucursal(nombre)){
             //Uso del simpleton y factory method
             FabricaSucursales fabrica = new FabricaSucursales();
             Sucursal sucursal = fabrica.crearSucursal(nombre);
             JuntaDirectiva junta = JuntaDirectiva.getJuntaDirectiva(fabrica);
+            
+            //Esta bdd es local, se necesita una global para poder usarla en verificaciones y registros
             BDD bdd = new BDD();
             bdd.addSucursal(sucursal);
             return true;
@@ -35,6 +42,7 @@ public class Ctrl_JuntaDirectiva {
     }
 
     public boolean ExisteMedico(Medico datos){
+        //Se tiene que verificar en bdd si existe el medico a registrar
         return true;
     }
 
