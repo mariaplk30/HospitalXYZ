c46dc20ed61bd6f87db2687c4a032c903b698cd2
freddygl3 <freddygl3@dhcppc3>
2022-03-20 00:18:36 -0400

    Inicio de Ctrl_Historia falta terminar

--- src/main/java/Controlador/Ctrl_Historia.java
+++ src/main/java/Controlador/Ctrl_Historia.java
@@ -1,11 +1,37 @@
 package Controlador;
+import Vista.Medico.*;
+
 import Modelo.Entidades.*;
+import Data.BDD;
+import java.util.*;
 
 public class Ctrl_Historia{
     
-    public Ctrl_Historia(){}
+    public Ctrl_Historia(){
+        
+    }
 
-    public void CrearHistoria(Paciente datos){
+    public void CrearHistoria(String paciente,String peso,String talla,String IMC,String tensionMax,String tensionMin){
+        BDD bd = new BDD();
+        bd.leerArchivoJSON();
+        ArrayList<Sucursal> sucursales = bd.getArregloSucursales();
+        
+        HistorialCita historialCita = new HistorialCita(id);
+        historialCita.setPeso(Double.parseDouble(peso));
+        historialCita.setTalla(Double.parseDouble(talla));
+        historialCita.setPesoTalla(Double.parseDouble(IMC));
+        historialCita.setTensionMax(Integer.parseInt(tensionMax));
+        historialCita.setTensionMin(Integer.parseInt(tensionMin));
+        int tession[]= {historialCita.getTensionMax(),historialCita.getTensionMin()};
+        historialCita.setTension(tession);
+        
+        Historia historia = new Historia();
+        historia.setID();
+        historia.setPaciente(paciente);
+        cita.setMedico(sucursales.get(SucursalI).getMedico(MedicoI).getID());
+
+        sucursales.get(SucursalI).getPaciente(PacienteI).addCita(cita);
+        bd.main();
     
     }
 
